26337ecebd0be8a9a805a4c6c0bebda94a4dc05b
AGABETAS <139512290+AGABETAS@users.noreply.github.com>
2023-07-26 17:06:41 +0300

    Add files via upload

---
+++ videos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>videos</title>
+</head>
+
+<body>
+<br /><br />
+<embed src="video/vid5.mp4" width="200" height="300"></embed>
+<embed src="video/vid4.mp4" width="200" height="300"></embed>
+<embed src="video/vid8.mp4" width="200" height="300"></embed>
+<embed src="video/vid2.mp4" width="200" height="300"></embed>
+<embed src="video/vid6.mp4" width="200" height="300"></embed>
+<br /><br />
+<li><a href="conduct_us.php"><i class="fa fa-home" aria-hidden="true"></i><em style="color:blue" > Click To Go Back</em></a></li>
+
+</body>
+</html>
